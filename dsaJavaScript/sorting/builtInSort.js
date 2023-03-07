f38418e32b6674ee5fdd2e4dd06e4be37b631b98
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2023-03-07 18:30:54 +0530

    sorted array using built.in method

---
+++ dsaJavaScript/sorting/builtInSort.js
@@ -0,0 +1,35 @@
+// const arr = [1,undefined, 20,, 0, -2, 4, 6];
+// arr.sort((a,b)=> b - a);
+// console.log(arr)
+
+// const items = [
+//     { name: "Edward", value: 21 },
+//     { name: "Sharpe", value: 37 },
+//     { name: "And", value: 45 },
+//     { name: "The", value: -12 },
+//     { name: "Magnetic", value: 13 },
+//     { name: "Zeros", value: 37 },
+//   ];
+
+// items.sort((a, b)=>{
+//     let x = a.name.toLocaleLowerCase();
+//     let y = b.name.toLocaleLowerCase();
+
+//     if(x > y){
+//         return -1;
+//     }
+//     if(x < y){
+//         return 1;
+//     }
+   
+//     return 0;
+// })
+// console.log(items)
+
+// const items = ["réservé", "premier", "communiqué", "café", "adieu", "éclair"];
+// console.log(items);
+// items.sort((a, b)=>a.localeCompare(b));
+// console.log(items)
+
+
+// the array to be sorted
